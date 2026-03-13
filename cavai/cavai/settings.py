c76d37a6e5da186e140ccd828e05d1cd913acb04
diogodebastos <diogodebastos18@gmail.com>
2026-03-13 13:41:58 +0000

    👈

--- cavai/cavai/settings.py
+++ cavai/cavai/settings.py
@@ -119,11 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
+
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'chatbot' / 'static',
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
